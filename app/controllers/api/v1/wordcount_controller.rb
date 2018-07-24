d8ed7b75d7cb3391a04190fea6cfd74a20e90324
Lee Azzarello <lee@rockingtiger.com>
2018-07-23 20:19:48 -0700

    word counting with response!

--- app/controllers/api/v1/wordcount_controller.rb
+++ app/controllers/api/v1/wordcount_controller.rb
@@ -8,4 +8,15 @@ class Api::V1::WordcountController < Api::V1::BaseController
   def echo
     render json: params['wordcount']
   end
+
+  def count
+    words = params['words'].split
+    count = words.size
+    wordlist = {}
+    words.each do |word|
+      wordlist[word] = 1
+    end
+    resp = {:count => count, :words => wordlist}
+    render json: resp
+  end
 end
