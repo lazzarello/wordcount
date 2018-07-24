a723d24f81018feae16d67b6bdba80d37f131feb
Lee Azzarello <lee@rockingtiger.com>
2018-07-23 19:58:47 -0700

    POST echo service

--- app/controllers/api/v1/wordcount_controller.rb
+++ app/controllers/api/v1/wordcount_controller.rb
@@ -4,4 +4,8 @@ class Api::V1::WordcountController < Api::V1::BaseController
     words = { count: "Hello World!"}
     render json: words.to_json
   end
+
+  def echo
+    render json: params['wordcount']
+  end
 end
